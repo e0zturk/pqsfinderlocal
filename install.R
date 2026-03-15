6e6548a7354303ef964959d516979bc3a9512416
e0zturk <emre.eusturk@gmail.com>
2026-03-15 19:21:10 +0300

    Update install.R

--- install.R
+++ install.R
@@ -10,5 +10,4 @@ options(renv.consent = TRUE)
 
 renv::restore(prompt = FALSE, exclude = c("Gviz", "BSgenome.Hsapiens.UCSC.hg38"))
 
-BiocManager::install("Gviz", ask = FALSE)
-BiocManager::install("BSgenome.Hsapiens.UCSC.hg38", ask = FALSE)
+
